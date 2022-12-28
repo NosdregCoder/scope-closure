afd9afad00f66146ea65e4da0a5c2f034194b0b3
Nosdreg <gerdson.icc@gmail.com>
2022-12-27 22:58:09 -0800

    Agregando los archivos finales

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+// var nameOfDog; //Undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`The best dog is ${elmo}`);
+}
+
+var elmo = 'Elmito'; 
